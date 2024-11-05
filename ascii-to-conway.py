f78fd90a8afd1d3096b923ea60c448718b1578f2
Lilly <lilly@MacBook-Pro.local>
2024-11-06 06:13:26 +1100

    better license text

--- ascii-to-conway.py
+++ ascii-to-conway.py
@@ -1,6 +1,11 @@
 # licensed under the AGPLv3 or later
-import sys, unicodedata
-import numpy as np
+"""
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+"""
 
 def read_between_markers(file_path, start_marker="STARTCHAR", end_marker="ENDCHAR", onlyascii = False):
     contents = {}
